3a0164e9deea191f59be33c4b8db1da2bedc20a8
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-06-11 21:54:41 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,44 @@
 // ==UserScript==
-// @name         Just Dance Now Pictos
+// @name         Just Dance Now Odd Or Even
 // @namespace    JDNAnuCore
 // @version      0.0.1
-// @description  Just Dance Now Pictos
+// @description  Add Odd Or Even In hud element
 // @author       ibratabian17
 // @match        https://justdancenow.com/
 // @icon         https://www.google.com/s2/favicons?domain=justdancenow.com
 // @grant        none
 // ==/UserScript==
-console.log("PB double started")
+console.log("JDNAnuCore oddoreven Started...")
+
 function PictoBar(){
 //PB Start
-console.log("PB double started")
+var odon = false;
+var prefbeatdur;
+var beatdur;
+
 setInterval(function(){
-  var checkExistClass = $(".js").hasClass("state-tutorial");
+  var checkExistClass = $(".js").hasClass("state-dance");
   if(checkExistClass ){
-  var fixedbeat = $('.song-action__button')
-  var beatdur = fixedbeat.css('animation-duration')
-  const root = document.querySelector(":root"); //grabbing the root element
-  console.log("PB patched")
-  root.style.setProperty("--pseudo-backgroundcolor", beatdur);
+    
+  fixedbeat = $('#beat')
+  beatdur = fixedbeat.css('animation-duration')
+  //const root = document.querySelector(":root"); grabbing the root element, this code has been deprecated
+  if (beatdur !== prefbeatdur){
+    if(odon ){
+      odon = false;
+      const hud = document.querySelector('.hud');
+      hud.classList.add("odd");
+      hud.classList.remove("even");
+    } else {
+      odon = true;
+      const hud = document.querySelector('.hud');
+      hud.classList.add("even");
+      hud.classList.remove("odd");
+    }
+  }
+  prefbeatdur = fixedbeat.css('animation-duration');
         }
-  }, 500);//run this thang every .5 seconds
+  }, 1);//run this thang every 1 miliseconds
 }
 
 PictoBar()
